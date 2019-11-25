f809c1a99e8222eaa5fb4c933cae5439dfcaafe0
Youngyun <youngyun.namkung@gmail.com>
2019-11-25 09:37:07 -0500

    Initial setting and design UI

--- App.js
+++ App.js
@@ -1,19 +1,84 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from "react-native";
+import Todo from "./ToDo"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const { height, width } = Dimensions.get("window");
+
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
+  render() { 
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Aurora, Do To Do</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New to do"} 
+            placeholderTextColor={"#999"}
+            value={newToDo} 
+            onChangeText={this._controlNewToDo}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.todo}>
+            <Todo />
+          </ScrollView>
+        </View>
+      </View>
+    )
+   };
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    })
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#654ea3",
+    alignItems: "center",
   },
+  title: {
+    color: "#ffffff",
+    fontSize: 30,
+    marginTop: 70,
+    marginBottom: 30,
+    fontWeight: "200",
+  },
+  card: {
+    backgroundColor: "#ffffff",
+    flex: 1,
+    width: width - 30,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.7,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width:0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 20
+  },
+  todo: {
+    alignItems: "center"
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Do To Do List
 Do To Do List App with React Native
+
+#
+
+```
+...Platform.select({
+  ios: {
+    shadowColor: "rgb(50, 50, 50)",
+    shadowOpacity: 0.7,
+    shadowRadius: 5,
+    shadowOffset: {
+      height: -1,
+      width:0
+    }
+  },
+  android: {
+    elevation: 3
+  }
+})
+```
